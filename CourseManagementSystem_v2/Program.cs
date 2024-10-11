dc9f28bc271449350bb3c1f10e4ef605e4a9c984
UT01635 <UT01635@UNICOMTIC26>
2024-10-11 15:58:20 +0530

    Q12  database and  table created

--- CourseManagementSystem_v2/Program.cs
+++ CourseManagementSystem_v2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,55 @@ namespace CourseManagementSystem_v2
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+
+
+
+
+        }
+
+        private readonly string connectionString = "server=(localdb)\\MSSQLLocalDB;database=master";
+        public void InitailizeDatabase()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string createDatabase = "IF NOT EXISTS (SELECT * FROM sys.database WHERE name ='CourseManagement')" +
+                                         "CREATE DATABASE CourseManagement";
+
+                using (SqlCommand cmd = new SqlCommand(createDatabase, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                con.ChangeDatabase("CourseManagement");
+
+                string createTable = @" 
+                    IF NOT EXISTS (SELECT  * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Courses')
+                    CREATE TABLE Courses(
+                        CourseId INT PRIMARY KEY,
+                        Title NVARCHAR(100),
+                        Duration  NVARCHAR(100),
+                        Price decimal(18, 2)
+                    );";
+
+                string InsertData = @"""INSERT INTO Courses (CourseId,Title,Duration,Price) 
+                                    VALUES (C_001 ,Java,6 Months, 1.00 );";
+
+                using (SqlCommand command = new SqlCommand(createTable, con))
+                {
+                    command.ExecuteNonQuery();
+                }
+                using (SqlCommand cmd = new SqlCommand(InsertData,con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
         }
     }
 }
